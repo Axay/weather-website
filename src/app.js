a9db4064fb060bc78eed576b35a919c4457a4315
Axay <akshayshailesh@gmail.com>
2022-02-27 21:10:05 +0530

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 //Define path for Express config
 const publicDiractoryPath = path.join(__dirname, '../public');
@@ -45,7 +46,7 @@ app.get('/help', (req,res) => {
 app.get('/weather', (req, res) => {
     if (!req.query.address){
         return res.send({
-            error : 'You must prodive address'
+            error : 'You must prodive location! '
         });
     }
     geocode(req.query.address, (error, {latitude, longitude, location} = {} ) => {
@@ -71,21 +72,6 @@ app.get('/weather', (req, res) => {
 });
 
 
-
-// app.get('/products', (req,res) => {
-//     if (!req.query.search) {
-//         return res.send({
-//             error : 'Invalid search'
-//         })
-//     }
-//     res.send({
-//         products : []
-//     });
-
-// });
-
-
-
 app.get('/help/*', (req, res) =>{
     res.render( '404.hbs', {
         title : '404',
@@ -103,6 +89,6 @@ app.get('*', (req, res) =>{
 });
 
 
-app.listen(3000, () =>{
-    console.log('Server is up on port 3000');
+app.listen(port, () =>{
+    console.log('Server is up on port ' + port );
 });
